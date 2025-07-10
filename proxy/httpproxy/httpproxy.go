4865a83bb605c76da802fab3609c7e0fa8154752
Mansour Behabadi <mbehabadi@mathspace.com.au>
2025-07-10 22:39:23 +1000

    Add HTTP proxy package with basic structure

--- proxy/httpproxy/httpproxy.go
+++ proxy/httpproxy/httpproxy.go
@@ -1,1 +1,27 @@
+// Package httpproxy provides a HTTP proxy implementation for a service.
 package httpproxy
+
+import (
+	"context"
+
+	"github.com/mathspace/zipnap/config"
+)
+
+type HTTPProxy struct {
+	cfg config.HTTP
+}
+
+func New(c config.HTTP) *HTTPProxy {
+	return &HTTPProxy{
+		cfg: c,
+	}
+}
+
+func (p *HTTPProxy) Start(waitHealthy func(ctx context.Context) error) error {
+}
+
+func (p *HTTPProxy) IsServiceHealthy(ctx context.Context, host string) (bool, error) {
+}
+
+func (p *HTTPProxy) Kill(ctx context.Context) error {
+}
