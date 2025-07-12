41229370c12212bf618135725e74f7a2f5691b59
Mansour Behabadi <mbehabadi@mathspace.com.au>
2025-07-12 20:33:37 +1000

    Add EC2 host implementation for host.Host interface

--- host/ec2host/ec2.go
+++ host/ec2host/ec2.go
@@ -1,1 +1,32 @@
+// Package ec2host provides an implementation of the host.Host interface for AWS
+// EC2 instances.
 package ec2host
+
+import (
+	"context"
+
+	"github.com/mathspace/zipnap/config"
+	"github.com/mathspace/zipnap/host"
+)
+
+type EC2Host struct {
+	cfg config.Instance
+}
+
+func New(cfg config.Instance) *EC2Host {
+	return &EC2Host{
+		cfg: cfg,
+	}
+}
+
+func (h *EC2Host) Start(ctx context.Context) error {
+	return nil
+}
+
+func (h *EC2Host) Stop(ctx context.Context) error {
+	return nil
+}
+
+func (h *EC2Host) Status(ctx context.Context) (host.Status, error) {
+	return host.StatusUnknown, nil
+}
